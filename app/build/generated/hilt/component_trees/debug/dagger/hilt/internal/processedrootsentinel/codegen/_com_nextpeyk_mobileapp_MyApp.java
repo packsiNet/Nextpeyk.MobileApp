b9d52f5bf9a0d7b479d34a5461e71f15132e7a8e
packsiNet <info@packsi.net>
2026-06-12 18:26:36 +0330

    update gitignore

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_nextpeyk_mobileapp_MyApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.nextpeyk.mobileapp.MyApp"
-)
-public final class _com_nextpeyk_mobileapp_MyApp {
-}
